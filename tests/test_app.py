3b9d8f24ae50f9be2ff4535181d0ff80b0884b19
José Daniel Man Castellanos <34019072+JoseMan200124@users.noreply.github.com>
2026-05-07 14:38:20 -0600

    Fix
    Fix lint errors, tests and security scan

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,9 +1,9 @@
-# test_app.py
+from src.app import restar, sumar
 
-from src.app import sumar, restar
 
 def test_sumar():
     assert sumar(2, 3) == 5
 
+
 def test_restar():
-    assert restar(5, 3) == 1
+    assert restar(5, 3) == 2
